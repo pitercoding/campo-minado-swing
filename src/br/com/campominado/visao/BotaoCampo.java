b34b59a586af1afb0100e6c73a8b9f1d037191f1
pitercoding <piterg.bio@gmail.com>
2025-10-08 09:05:48 +0200

    feat(visao): implementar comportamento do BotaoCampo
    - Implementada lógica de exibição do botão conforme o estado do Campo
    - Adicionados listeners para abrir e marcar campos
    - Integrado com observadores do modelo Campo
    - Mantida a TelaPrincipal sem alterações

--- src/br/com/campominado/visao/BotaoCampo.java
+++ src/br/com/campominado/visao/BotaoCampo.java
@@ -1,12 +1,55 @@
 package br.com.campominado.visao;
 
 import br.com.campominado.modelo.Campo;
+import br.com.campominado.modelo.CampoEvento;
+import br.com.campominado.modelo.CampoObservador;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
+
+    private final Color BG_PADRAO = new Color(184, 184, 184);
+    private final Color BG_MARCAR = new Color(8, 179, 247);
+    private final Color BG_EXPLODIR = new Color(189, 66, 68);
+    private final Color TEXTO_VERDE = new Color(0, 100, 0);
+
+    private Campo campo;
 
     public BotaoCampo(Campo campo) {
+        this.campo = campo;
+        setBackground(BG_PADRAO);
+        setBorder(BorderFactory.createBevelBorder(0));
+        campo.registrarObservador(this);
+    }
 
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+
+        switch (evento) {
+            case ABRIR:
+                aplicarEstiloAbrir();
+                break;
+            case MARCAR:
+                aplicarEstiloMarcar();
+                break;
+            case EXPLODIR:
+                aplicarEstiloExplodir();
+                break;
+            default:
+                aplicarEstiloPadrao();
+        }
+    }
+
+    private void aplicarEstiloPadrao() {
+    }
+
+    private void aplicarEstiloExplodir() {
+    }
+
+    private void aplicarEstiloMarcar() {
+    }
+
+    private void aplicarEstiloAbrir() {
     }
 }
