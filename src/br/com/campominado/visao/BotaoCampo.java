bf65a4bf89701b4bbb4c903f7b85a3950438c867
pitercoding <piterg.bio@gmail.com>
2025-10-08 08:54:07 +0200

    public int getLinhas() {         return linhas;     }      public int getColunas() {         return colunas;     }
    - Criada classe BotaoCampo estendendo JButton
    - PainelTabuleiro agora monta grid dinamicamente com base em Tabuleiro
    - Adicionado método Tabuleiro.paraCadaCampo(Consumer<Campo>) para iterar sobre os campos
    - Definidos atributos finais linhas, colunas e minas em Tabuleiro, com respectivos getters
    - Preparado observador do Tabuleiro para exibir resultados futuros

---
+++ src/br/com/campominado/visao/BotaoCampo.java
@@ -0,0 +1,12 @@
+package br.com.campominado.visao;
+
+import br.com.campominado.modelo.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+
+    }
+}
--- src/br/com/campominado/visao/PainelTabuleiro.java
+++ src/br/com/campominado/visao/PainelTabuleiro.java
@@ -3,10 +3,18 @@ package br.com.campominado.visao;
 import br.com.campominado.modelo.Tabuleiro;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PainelTabuleiro extends JPanel {
 
     public PainelTabuleiro(Tabuleiro tabuleiro) {
 
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+
+        tabuleiro.registrarObservador(e -> {
+            // TODO mostrar resultado pro usuário
+        });
     }
 }
