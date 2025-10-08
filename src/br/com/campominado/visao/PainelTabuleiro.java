366b9adc1506a2b536d20c6ed3c2aa4aaf1007de
pitercoding <piterg.bio@gmail.com>
2025-10-08 08:36:45 +0200

    feat(visao): criar TelaPrincipal e PainelTabuleiro para interface inicial do jogo
    - Adicionada classe TelaPrincipal com JFrame configurado (título, tamanho e encerramento)
    - Criada classe PainelTabuleiro que receberá o objeto Tabuleiro
    - Integração inicial entre modelo (Tabuleiro) e visão

---
+++ src/br/com/campominado/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.campominado.visao;
+
+import br.com.campominado.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
