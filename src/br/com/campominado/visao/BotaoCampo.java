65d3b4c4b751a91fdaad622a3cc3e0f20623ba70
pitercoding <piterg.bio@gmail.com>
2025-10-08 09:28:39 +0200

    feat(visao): implementar lógica visual e interação do BotaoCampo
    - Adicionado comportamento de observador (CampoObservador)
    - Implementados estilos visuais para abrir, marcar e explodir campos
    - Incluído tratamento de clique do mouse (abrir com botão esquerdo, marcar com direito)
    - Integrado BotaoCampo com o modelo Campo via eventos CampoEvento

--- src/br/com/campominado/visao/BotaoCampo.java
+++ src/br/com/campominado/visao/BotaoCampo.java
@@ -6,8 +6,10 @@ import br.com.campominado.modelo.CampoObservador;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class BotaoCampo extends JButton implements CampoObservador {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
 
     private final Color BG_PADRAO = new Color(184, 184, 184);
     private final Color BG_MARCAR = new Color(8, 179, 247);
@@ -20,6 +22,8 @@ public class BotaoCampo extends JButton implements CampoObservador {
         this.campo = campo;
         setBackground(BG_PADRAO);
         setBorder(BorderFactory.createBevelBorder(0));
+
+        addMouseListener(this);
         campo.registrarObservador(this);
     }
 
@@ -51,5 +55,52 @@ public class BotaoCampo extends JButton implements CampoObservador {
     }
 
     private void aplicarEstiloAbrir() {
+        setBackground(BG_PADRAO);
+        setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+        switch (campo.minasNaVizinhanca()) {
+            case 1:
+                setForeground(TEXTO_VERDE);
+                break;
+            case 2:
+                setForeground(Color.BLUE);
+                break;
+            case 3:
+                setForeground(Color.YELLOW);
+                break;
+            case 4:
+            case 5:
+            case 6:
+                setForeground(Color.RED);
+                break;
+            default:
+                setForeground(Color.PINK);
+        }
+
+        String valor = !campo.vizinhancaSegura() ? campo.minasNaVizinhanca() + "" : "";
+
+        setText(valor);
     }
+
+    // Interface eventos do Mouse
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() == 1) {
+            campo.abrir();
+        } else {
+            campo.alternarMarcacao();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
 }
